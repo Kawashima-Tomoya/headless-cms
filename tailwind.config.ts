2adf520bd68c6b1592a4cbb36d2696a2b1d69328
Kawashima-Tomoya <57kawashima@gmail.com>
2024-09-15 12:53:04 +0900

    🔧 設定ファイルの更新

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,19 @@ const config: Config = {
 		"./app/**/*.{js,ts,jsx,tsx,mdx}",
 	],
 	theme: {
+		screens: {
+			xl: { max: "1279px" },
+			// => @media (max-width: 1279px) { ... }
+
+			lg: { max: "1023px" },
+			// => @media (max-width: 1023px) { ... }
+
+			md: { max: "767px" },
+			// => @media (max-width: 767px) { ... }
+
+			sm: { max: "639px" },
+			// => @media (max-width: 639px) { ... }
+		},
 		extend: {
 			backgroundImage: {
 				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
